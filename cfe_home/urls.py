609cc1257f4df40c1392c815d1652a80e52772f4
Muhammad Khuzaim <huzaimthegenius1234@gmail.com>
2025-11-06 19:52:06 +0500

    github oath

--- cfe_home/urls.py
+++ cfe_home/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("login/", auth_views.login_view, name="login"),
+    path("protected/", views.pw_protected_view, name="protected"),
+    path("user-only/", views.user_only_view, name="user_only"),
 ]
--- cfe_home/views.py
+++ cfe_home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model 
+from django.contrib.auth.decorators import  login_required
 
 User = get_user_model()
 
@@ -10,3 +11,14 @@ def home(request):
     else:
         print("User is not authenticated")
     return render(request, "home.html")
+
+def pw_protected_view(request):
+    is_allowed=False
+    if is_allowed:
+        return render(request, "protected/view.html")
+    return render(request, "protected/entry.html")
+
+
+@login_required
+def user_only_view(request):
+    return render(request, "protected/user_only.html", {"username": request.user.username})
